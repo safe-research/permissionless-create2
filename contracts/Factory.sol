0185c3f574eaae8c5deec550a49a5247d2e1e0d9
Nicholas Rodrigues Lordello <nick@safe.global>
2025-05-26 18:09:53 +0200

    Use `RETURNDATASIZE` Instead of `PUSH0`
    In order to not depend on EIP-3855 `PUSH0` opcode, we refactor the
    factory code to use `RETURNDATASIZE` which is guaranteed to be
    `0` **except** for when `CREATE2` reverts.
    
    With a little bit of moving code around, we also managed to change
    make the code one byte smaller and fit into a 32-byte word.

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -6,10 +6,10 @@ pragma solidity ^0.8.29;
 ///      and this library exports constants for use in Solidity.
 library Factory {
     address internal constant DEPLOYER = 0x962560A0333190D57009A0aAAB7Bfa088f58461C;
-    bytes32 internal constant SALT = hex"0000000000000000000000000000000000000000000000000000000000057a67";
-    address internal constant ADDRESS = 0xC0DE8E984dF1846E6AdE500972641ce0a9669e1b;
+    bytes32 internal constant SALT = hex"000000000000000000000000000000000000000000000000000000000019e078";
+    address internal constant ADDRESS = 0xC0DE945918F144DcdF063469823a4C51152Df05D;
 
-    bytes internal constant INITCODE = hex"7860205f3581360380835f375f34f58060145790fd5b5f525ff35f5260196007f3";
-    bytes internal constant RUNCODE = hex"60205f3581360380835f375f34f58060145790fd5b5f525ff3";
-    bytes32 internal constant CODEHASH = hex"f3ee84f262524054463f2deab3d163dd9217d59231bd95b3b39df74b998cda6e";
+    bytes internal constant INITCODE = hex"7760203d3d3582360380843d373d34f580601457fd5b3d52f33d5260186008f3";
+    bytes internal constant RUNCODE = hex"60203d3d3582360380843d373d34f580601457fd5b3d52f3";
+    bytes32 internal constant CODEHASH = hex"eac13dde1a2c9b8dc8a7aded29ad0af5d57c811b746f7909ea841cbfc6ef3adc";
 }
