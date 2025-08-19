5ee558a5becc45b974aebd4130e0dbbdc8b48bd1
Nicholas Rodrigues Lordello <n@lordello.net>
2025-08-19 14:31:25 +0200

    Propagate Revert Data (#12)
    This PR changes the permissionless CREATE2 factory to propagate revert
    data (like [EIP-7997](https://github.com/ethereum/EIPs/pull/10092))
    instead of just reverting with empty data.
    
    Note that we no longer have the init code fiting into 32-byte :'(, but
    the runtime code still does :). That being said, the usability
    improvement of propagating revert data is worth the extra code size IMO.
    Note that deployment gas costs _still remain the same_, as the extra
    code is just in the `revert` branch for contract deployments.
    
    I also chose to keep `calldatasize() - 32` overflow behaviour as it is
    now. That is, it will revert with OOG (from trying to copy ~2**256 bytes
    of calldata to memory) instead of handling the revert manually. The
    rationale here is:
    
    1. The call reverts one way or the other, so we don't have bad state
    (different from the suggestion in EIP-7997 which uses a saturating
    subtraction, where incorrect deployments will silently fail)
    2. Additional gas costs would be paid on **every** create2 deployment to
    handle the conditional jump (basically, the "if calldatasize() < 32"
    check), for arguably very little benefit: it will only save gas for
    incorrect invocations, which shouldn't be used anywhere anyway.
    
    The corresponding ERC changes are included here:
    https://github.com/safe-research/ERCs/pull/5

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -6,10 +6,10 @@ pragma solidity ^0.8.29;
 ///      and this library exports constants for use in Solidity.
 library Factory {
     address internal constant DEPLOYER = 0x962560A0333190D57009A0aAAB7Bfa088f58461C;
-    bytes32 internal constant SALT = hex"000000000000000000000000000000000000000000000000000000000019e078";
-    address internal constant ADDRESS = 0xC0DE945918F144DcdF063469823a4C51152Df05D;
+    bytes32 internal constant SALT = hex"000000000000000000000000000000000000000000000000000000000001bec5";
+    address internal constant ADDRESS = 0xC0DEb853af168215879d284cc8B4d0A645fA9b0E;
 
-    bytes internal constant INITCODE = hex"7760203d3d3582360380843d373d34f580601457fd5b3d52f33d5260186008f3";
-    bytes internal constant RUNCODE = hex"60203d3d3582360380843d373d34f580601457fd5b3d52f3";
-    bytes32 internal constant CODEHASH = hex"eac13dde1a2c9b8dc8a7aded29ad0af5d57c811b746f7909ea841cbfc6ef3adc";
+    bytes internal constant INITCODE = hex"7c60203d3d3582360380843d373d34f5806019573d813d933efd5b3d52f33d52601d6003f3";
+    bytes internal constant RUNCODE = hex"60203d3d3582360380843d373d34f5806019573d813d933efd5b3d52f3";
+    bytes32 internal constant CODEHASH = hex"2ad75e1e9642e6fce7d293d52fa5a8f62a79a2079abb7402256add02d6e8bc30";
 }
