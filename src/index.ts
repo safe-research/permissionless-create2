d1bbd6a7268799f4a70fdb42ec4b32a28fd72d1d
Nicholas Rodrigues Lordello <n@lordello.net>
2025-08-20 23:47:55 +0200

    Migrate to Hardhat 3
    This PR migrates to Hardhat 3 + Viem. Latest hardhat now implements
    EIP-7702 support, which allows us to run unit tests for the
    permissionless CREATE2 factory contract.

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+const deployer = Object.freeze({
+  address: "0x962560A0333190D57009A0aAAB7Bfa088f58461C",
+  mnemonic: "make code code code code code code code code code code coconut",
+  privateKey:
+    "0x942ba639ec667bdded6d727ad2e483648a34b584f916e6b826fdb7b512633731",
+});
+
+const factory = Object.freeze({
+  address: "0xC0DEb853af168215879d284cc8B4d0A645fA9b0E",
+  initCode:
+    "0x7c60203d3d3582360380843d373d34f5806019573d813d933efd5b3d52f33d52601d6003f3",
+  salt: "0x000000000000000000000000000000000000000000000000000000000001bec5",
+  runtimeCode: "0x60203d3d3582360380843d373d34f5806019573d813d933efd5b3d52f3",
+  codeHash:
+    "0x2ad75e1e9642e6fce7d293d52fa5a8f62a79a2079abb7402256add02d6e8bc30",
+});
+
+export { deployer, factory };
